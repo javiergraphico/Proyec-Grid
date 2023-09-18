e3e5efc00049047d655a841c1acfd396aed67464
javiergraphico <javieralexander38@hotmail.com>
2023-09-17 21:36:47 -0400

    animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 ScrollReveal().reveal('.grid', {
   delay: 200,
   origin: 'top',
-  distance: '2000px',
+  distance: '1500px',
   duration: 2000,
-  opacity: 0.5
+  opacity: 0.4,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 2)',
 
 });
