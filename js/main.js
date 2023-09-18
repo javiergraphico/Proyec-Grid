af09abb119c1ebf3093985763fe35f258faa7ee8
javiergraphico <javieralexander38@hotmail.com>
2023-09-18 17:47:51 -0400

    cambio de carrusel

--- js/main.js
+++ js/main.js
@@ -7,11 +7,39 @@ ScrollReveal().reveal('.grid', {
   easing: 'cubic-bezier(0.4, 0.2, 0.1, 2)',
 
 });
+//fin de animacion
 
-
+// evento modo oscuro
 const swith = document.querySelector('.container_toogle');
 
 swith.addEventListener('click', () => {
   swith.classList.toggle('active');
   document.body.classList.toggle('active');
 })
+
+
+
+ // Swiper slider
+ const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  direction: "horizontal",
+  loop: false,
+  
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
